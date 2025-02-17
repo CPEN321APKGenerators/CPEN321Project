2fb9b355ecf16b4c3e318e324b09e9864a0e350f
christinejg <christinejiang02@gmail.com>
2025-02-16 18:17:04 -0800

    xml for profile

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.cpen321project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.cpen321project"
--- Frontend/app/src/main/java/com/example/cpen321project/ProfileManagement.kt
+++ Frontend/app/src/main/java/com/example/cpen321project/ProfileManagement.kt
@@ -1,29 +1,25 @@
 package com.example.cpen321project
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+
+class ProfileManagement : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile_management)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_view)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<Button>(R.id.profile_button).setOnClickListener() {
-            val intent = Intent(this, ProfileManagement::class.java)
-            startActivity(intent)
-        }
     }
-
-
 }
