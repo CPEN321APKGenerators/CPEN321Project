36fb1c8d22327c30810cffc3078229837dd12d8c
Nyi <nyinyi.ece@gmail.com>
2025-03-01 17:27:33 -0800

    Rasa Endpoint Updated

--- Backend/rasa_bot/actions/actions.py
+++ Backend/rasa_bot/actions/actions.py
@@ -1,27 +1,42 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from datetime import datetime
+import logging
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import List, Dict, Any
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionSaveJournalEntry(Action):
+    
+    def name(self) -> str:
+        return "action_save_journal_entry"
+    
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> List[Dict[str, Any]]:
+        journal_entry = tracker.get_slot("journal_entry")
+        current_date = datetime.now().strftime('%Y-%m-%d') 
+        user_id = "1" #temp 
+        
+        if journal_entry and user_id:
+            response = self.save_journal_entry(user_id, journal_entry, current_date)
+            if response.status_code == 200:
+                dispatcher.utter_message(text="Your journal entry has been saved successfully!")
+            else:
+                dispatcher.utter_message(text="There was an error saving your journal entry.")
+        else:
+            dispatcher.utter_message(text="No journal entry provided.")
+        return []
+    
+    def save_journal_entry(self, user_id: str, journal_entry: str, date: str) -> requests.Response:
+        url = "http://ec2-35-183-201-213.ca-central-1.compute.amazonaws.com:5000/api/journal" 
+        data = {
+            "userID": user_id,  
+            "text": journal_entry,
+            "date": date  
+        }
+        
+        try:
+            # Send POST request to backend API
+            response = requests.post(url, json=data)
+            return response
+        except Exception as e:
+            logging.error(f"Failed to save journal entry: {e}")
+            return None
