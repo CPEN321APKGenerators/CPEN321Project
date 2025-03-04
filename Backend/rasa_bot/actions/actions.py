6aa990d9207a17acaf5349ad3b876ffd69972468
Nyi <nyinyi.ece@gmail.com>
2025-03-03 23:31:01 -0800

    RASA Date From FrontEnd, Change to BASE URL /api/journal

--- Backend/rasa_bot/actions/actions.py
+++ Backend/rasa_bot/actions/actions.py
@@ -1,9 +1,8 @@
-from datetime import datetime
-import logging
-import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import List, Dict, Any
+import requests
+import logging
 
 class ActionSaveJournalEntry(Action):
     
@@ -11,14 +10,14 @@ class ActionSaveJournalEntry(Action):
         return "action_save_journal_entry"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> List[Dict[str, Any]]:
-        # Get the journal entry, date, and user ID from slots
         journal_entry = tracker.get_slot("journal_entry")
-        entry_date = datetime.now().strftime('%Y-%m-%d')  
+        entry_date = tracker.get_slot("date")  # Getting date from frontend
         user_id = tracker.get_slot("userID")
-        user_google_token = tracker.get_slot("google_token")  
+        user_google_token = tracker.get_slot("google_token")
 
         print(user_id, entry_date, journal_entry)
-        if journal_entry and user_id:
+        
+        if journal_entry and user_id and entry_date:
             response = self.save_journal_entry(user_id, journal_entry, entry_date, user_google_token)
             if response and response.status_code == 200:
                 dispatcher.utter_message(text="Your journal entry has been saved successfully!")
@@ -30,10 +29,13 @@ class ActionSaveJournalEntry(Action):
         return []
     
     def save_journal_entry(self, user_id: str, journal_entry: str, entry_date: str, user_google_token: str) -> requests.Response:
-        url = "http://ec2-35-183-201-213.ca-central-1.compute.amazonaws.com:5000/api/journal"
+        #Using BASE URL
+        BASE_URL = "https://cpen321project-journal.duckdns.org"
+        url = f"{BASE_URL}/api/journal"
         
+        # Prepare the data to send
         data = {
-            "date": entry_date,  # Ensure date is in ISO8601 format
+            "date": entry_date,  # The date comes from the slot
             "userID": user_id,  
             "text": journal_entry,
         }
