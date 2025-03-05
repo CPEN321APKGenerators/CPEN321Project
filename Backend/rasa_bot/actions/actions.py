0bd244ef6d7d991050ee26d5337d030e954dcc12
Nyi <nyinyi.ece@gmail.com>
2025-03-04 16:19:37 -0800

    RASA send msg error to db fix

--- Backend/rasa_bot/actions/actions.py
+++ Backend/rasa_bot/actions/actions.py
@@ -1,8 +1,9 @@
+import logging
+import requests
+from datetime import datetime
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import List, Dict, Any
-import requests
-import logging
 
 class ActionSaveJournalEntry(Action):
     
@@ -10,12 +11,13 @@ class ActionSaveJournalEntry(Action):
         return "action_save_journal_entry"
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> List[Dict[str, Any]]:
+        # Get the journal entry, date, and user ID from slots
         journal_entry = tracker.get_slot("journal_entry")
         entry_date = tracker.get_slot("date")  # Getting date from frontend
         user_id = tracker.get_slot("userID")
         user_google_token = tracker.get_slot("google_token")
 
-        print(user_id, entry_date, journal_entry)
+        logging.debug(f"Received Journal Entry: {journal_entry}, Date: {entry_date}, UserID: {user_id}")
         
         if journal_entry and user_id and entry_date:
             response = self.save_journal_entry(user_id, journal_entry, entry_date, user_google_token)
@@ -29,25 +31,24 @@ class ActionSaveJournalEntry(Action):
         return []
     
     def save_journal_entry(self, user_id: str, journal_entry: str, entry_date: str, user_google_token: str) -> requests.Response:
-        #Using BASE URL
         BASE_URL = "https://cpen321project-journal.duckdns.org"
         url = f"{BASE_URL}/api/journal"
         
-        # Prepare the data to send
         data = {
-            "date": entry_date,  # The date comes from the slot
-            "userID": user_id,  
-            "text": journal_entry,
+            "date": entry_date,  
+            "userID": user_id,   
+            "text": journal_entry,  
+            "media": []  #(testToMatchJSON)
         }
 
-        # Prepare the request body with headers
         headers = {
-            "Authorization": f"Bearer {user_google_token}",  # Include Authorization token
+            "Authorization": f"Bearer {user_google_token}",
             "Content-Type": "application/json"
         }
         
         try:
             # Send POST request to backend API
+            logging.debug(f"Sending data to backend: {data}")
             response = requests.post(url, json=data, headers=headers)
             if response.status_code != 200:
                 logging.error(f"Failed to save journal entry. Status code: {response.status_code}")
