3c3554bd8686a677a759e3dc72b7881a1142e40b
Nyi <nyinyi.ece@gmail.com>
2025-03-13 12:39:23 -0700

    RASA NODEJS API HTTPS

--- Backend/rasa_api/server.js
+++ Backend/rasa_api/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
