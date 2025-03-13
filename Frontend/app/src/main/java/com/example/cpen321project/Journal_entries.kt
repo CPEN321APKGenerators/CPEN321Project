f94dbfee37a7678a489bcb6f65cbd12010555383
Nyi <nyinyi.ece@gmail.com>
2025-03-13 12:25:19 -0700

    RASABOT NODEJS WRAPPER

--- Frontend/app/src/main/java/com/example/cpen321project/Journal_entries.kt
+++ Frontend/app/src/main/java/com/example/cpen321project/Journal_entries.kt
@@ -64,7 +64,7 @@ class Journal_entries : AppCompatActivity() {
     private lateinit var sendChatButton: Button
     private val client = OkHttpClient()
     private val chatbotUrl =
-        "http://54.234.28.190:3001/api/chat"  #will change to https later
+        "http://54.234.28.190:3001/api/chat"  //will change to https later
     private val BASE_URL = "https://cpen321project-journal.duckdns.org"
     private var userID: String? = null
     private var user_google_token: String? = null
